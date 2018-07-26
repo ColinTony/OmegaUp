b256890c2e826503407e7981d8ea4032c998f6c8
ColinTonyDev <colincitoheredia@gmail.com>
2018-07-26 14:44:13 -0500

    alicia

---
+++ Alicia/Alicia.c
@@ -0,0 +1,13 @@
+/*
+	Autor: Colin Heredia Luis Antonio
+	Versión 1.0
+	Descripción: Solucion al problema Alicia y las llaves doradas (OmegaUP)
+*/ 
+
+#include <"stdio.h">
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
